f11f36064c64a3c9adf6324f2195815a7a948e37
Nimjii <lspreitzer525@gmail.com>
2025-08-23 22:19:06 +0200

    add useful nvim mappings

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -4,6 +4,7 @@
 require('which-key').add({
   { '<leader>f', group = 'Find' },
   { '<leader>g', group = 'Git' },
+  { '<leader>gc', group = 'Conflicts' },
   { '<leader><S-q>', group = 'Quickfix', icon = ' ' },
 })
 
@@ -19,7 +20,7 @@ vim.keymap.set({ 'n', 'v' }, '<C-u>', '<C-u>zz', { silent = true })
 vim.keymap.set({ 'n', 'v' }, '{', '{zz', { silent = true })
 vim.keymap.set({ 'n', 'v' }, '}', '}zz', { silent = true })
 vim.keymap.set({ 'n', 'v' }, 'G', 'Gzz', { silent = true })
-vim.keymap.set({ 'n' }, '*', '*zzzv', { silent = true })
+vim.keymap.set({ 'n' }, '*', '*N', { remap = true, silent = true })
 vim.keymap.set({ 'n' }, 'n', 'nzzzv', { silent = true })
 vim.keymap.set({ 'n' }, 'N', 'Nzzzv', { silent = true })
 vim.keymap.set({ 'n', 'v' }, 'x', '"_x', { desc = 'Delete character', silent = true })
@@ -33,6 +34,17 @@ vim.keymap.set({ 'n' }, '<C-Left>', '<cmd>vertical resize -2<CR>', { desc = 'Res
 vim.keymap.set({ 'n' }, '<C-Right>', '<cmd>vertical resize +2<CR>', { desc = 'Resize split right' })
 vim.keymap.set({ 'n' }, '<C-s>', '<C-w>s', { desc = 'Create horizontal split' })
 vim.keymap.set({ 'n' }, '<C-x>', '<C-w>v', { desc = 'Create vertical split' })
+vim.keymap.set({ 'n' }, '<esc>', '<cmd>nohlsearch<cr>', { silent = true })
+vim.keymap.set({ 'n' }, 'yc', 'yygccp', { remap = true, desc = 'Duplicate line and comment original' })
+vim.keymap.set({ 'n' }, '<leader>y', '"+y', { remap = true, desc = 'Yank to system clipboard' })
+vim.keymap.set({ 'n' }, '<leader>d', '_d', { desc = 'Delete (black hole register)' })
+
+-- Git conflict mappings
+vim.keymap.set({ 'n' }, '<leader>gcf', '/<<<<<<<<CR>', { desc = 'Find conflicts' })
+vim.keymap.set({ 'n' }, '<leader>gch', 'dd/=======<CR>0v/>>>>>>><CR>$x', { desc = 'Choose HEAD' })
+vim.keymap.set({ 'n' }, '<leader>gcu', '0v/=======<CR>$x/>>>>>>><CR>dd', { desc = 'Choose upstream' })
+vim.keymap.set({ 'n' }, '<leader>gcb', 'dd/=======<CR>dd/>>>>>>><CR>dd', { desc = 'Choose both' })
+vim.keymap.set({ 'n' }, '<leader>gcn', '0v/>>>>>>><CR>$x', { desc = 'Choose neither' })
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
