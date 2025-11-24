83db1f50ace45d06d1b8948790cbb80933d59bf9
Nimjii <lspreitzer525@gmail.com>
2025-11-24 18:05:18 +0100

    configure jdtls

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -86,9 +86,42 @@ return {
     local servers = {
       clangd = {},
       gopls = {},
-      jdtls = {},
       rust_analyzer = {},
       ts_ls = {},
+
+      jdtls = {
+        cmd = { 'jdtls', '-data', vim.fn.getcwd() },
+        init_options = {
+          extendedClientCapabilities = {
+            classFileContentsSupport = true,
+            generateToStringPromptSupport = true,
+            hashCodeEqualsPromptSupport = true,
+            advancedExtractRefactoringSupport = true,
+            advancedOrganizeImportsSupport = true,
+            generateConstructorsPromptSupport = true,
+            generateDelegateMethodsPromptSupport = true,
+            moveRefactoringSupport = true,
+            overrideMethodsPromptSupport = true,
+            executeClientCommandSupport = true,
+            inferSelectionSupport = {
+              "extractMethod",
+              "extractVariable",
+              "extractConstant",
+              "extractVariableAllOccurrence",
+            },
+          },
+        },
+        root_markers = { '.git', 'gradlew', 'mvnw' },
+        settings = {
+          java = {
+            configuration = {
+              runtimes = {
+                { name = "JavaSE-25", path = '/usr/lib/jvm/java-25-openjdk/' },
+              },
+            },
+          },
+        },
+      },
 
       lua_ls = {
         settings = {
